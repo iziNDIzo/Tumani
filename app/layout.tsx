6e8e0671f4c9bd6517f2f6cee12fa936b418c42c
iziNDIzo <thatuxg@gmail.com>
2026-09-20 21:01:49 +0200

    fix double header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import "./globals.css"
-import Header from "./components/header"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-white text-black antialiased">
-        <Header />
+       
         {children}
       </body>
     </html>
